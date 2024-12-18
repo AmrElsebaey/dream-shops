b3611031a1f53a4215e76f991178d1e75f105ddb
Amr Elsebay <amrelsebay3@gmail.com>
2024-12-18 08:57:42 +0200

    image controller

---
+++ src/main/java/com/smartsolution/dreamshops/controller/ImageController.java
@@ -0,0 +1,48 @@
+package com.smartsolution.dreamshops.controller;
+
+import com.smartsolution.dreamshops.dto.ImageDto;
+import com.smartsolution.dreamshops.model.Image;
+import com.smartsolution.dreamshops.response.ApiResponse;
+import com.smartsolution.dreamshops.service.image.IImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RestController
+@RequestMapping("${api.prefix}/images")
+@RequiredArgsConstructor
+public class ImageController {
+    private final IImageService imageService;
+
+    @RequestMapping("/upload")
+    public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files, @RequestParam Long producerId) {
+        try {
+            List<ImageDto> imageDtos = imageService.addImages(files, producerId);
+            return ResponseEntity.ok(new ApiResponse("Upload success", imageDtos));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).
+                    body(new ApiResponse("Upload failed", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage (@PathVariable Long imageId) throws SQLException {
+        Image image= imageService.getImageById(imageId);
+        ByteArrayResource resource= new ByteArrayResource(
+                image.getImage().getBytes(1,(int) image.getImage().length()));
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"").
+                body(resource);
+    }
+}
